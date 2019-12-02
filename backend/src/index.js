4ab54467e835801e4e8581e20b34d8b9bb7fcddf
Lucas <lhamancio@gmail.com>
2019-12-02 20:42:08 -0300

    .

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const uri = 'mongodb+srv://lucas:amancio@cluster0-ipohv.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(uri, { useNewUrlParser: true })
+
+app.use((req, res, next) => {
+    req.io = io
+    next()
+})
+
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'upload', 'resized')))
+app.use(require('./routes'))
+
+server.listen(3000)
+
+console.log('servidor rodando!')
